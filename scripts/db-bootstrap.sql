a3e4d4addca3493f43c61e6dc74391f5180c308a
danielfdez17 <daniel17dev@gmail.com>
2026-03-27 11:08:59 +0100

    feat(phase1): Add tests to validate phase1 of kong api

--- scripts/db-bootstrap.sql
+++ scripts/db-bootstrap.sql
@@ -1,5 +1,27 @@
 CREATE SCHEMA IF NOT EXISTS auth;
 
+ALTER ROLE postgres IN DATABASE postgres SET search_path = auth, public;
+
+SELECT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'anon') AS anon_role_exists
+\gset
+
+\if :anon_role_exists
+\else
+CREATE ROLE anon NOLOGIN;
+\endif
+
+SELECT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'authenticated') AS authenticated_role_exists
+\gset
+
+\if :authenticated_role_exists
+\else
+CREATE ROLE authenticated NOLOGIN;
+\endif
+
+GRANT USAGE ON SCHEMA public TO anon, authenticated;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO anon, authenticated;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO anon, authenticated;
+
 SELECT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'supabase_admin') AS role_exists
 \gset
 
